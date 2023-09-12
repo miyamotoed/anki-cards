d952f3a08bd14fd831dd8ba9a4a642dd0b294dcf
Miyamoto <miyamoto@example.com>
2023-09-11 21:34:00 -0600

    add cards: postgres

--- sql/postgres.md
+++ sql/postgres.md
@@ -10,9 +10,11 @@
 
 <!-- notecardId: 1691717853083 -->
 
-
 <img src="https://raw.githubusercontent.com/miyamotoed/anki-cards/master/sql/images/postgres_update_subquery.png">
 
+## DELETE statement with USING clause
+
+<img src="https://raw.githubusercontent.com/miyamotoed/anki-cards/master/sql/images/postgres_delete_with_using_clause.png">
 ## Update
 
 <!-- notecardId: 1691808588199 -->
--- sql/postgres.sql
+++ sql/postgres.sql
@@ -7,18 +7,19 @@ REPLACE (
     );
 
 -- Update duplicate rows using subquery
-UPDATE
-    jutsu
-SET
-	deleted_at = now()
-WHERE id
-	NOT IN (
-    SELECT
-        max(id)
-    FROM
-        jutsu
-    WHERE
-        deleted_at IS NULL
-    GROUP BY
-        UPPER (nombre) 
+
+UPDATE jutsu
+SET deleted_at = now()
+WHERE id NOT IN (
+        SELECT max(id)
+        FROM jutsu
+        WHERE deleted_at IS NULL
+        GROUP BY UPPER (nombre)
     );
+
+-- DELETE statement with USING clause
+
+DELETE FROM
+    jutsu USING forbidden_jutsus
+WHERE
+    jutsu.id = forbidden_jutsus.id
